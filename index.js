0236292477f6952c040846556e3dc58d66acba10
Anchit Lahkar <64302945+Anchitlahkar@users.noreply.github.com>
2022-12-22 16:51:01 +0530

    22.12.2022

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 $(document).ready(function () {
-    $("#submit").click(()=>{
+    $("#submit").click(() => {
         val = document.getElementById("input-text").value
         openai_test(val)
     })
 })
+
+function text_change(){
+    val = document.getElementById("input-text").value
+
+    if(val !== ""){
+        $("#input-text").css("font-style","normal")
+    }
+    else{
+        $("#input-text").css("font-style","italic")
+    }
+}
+
+setInterval(text_change, 500);
 
 var result = ""
 
@@ -12,14 +25,14 @@ function stretchy(element) {
 
     function update() {
         var h = element.scrollHeight;
-        if (h>element.offsetHeight || h<element.offsetHeight-48)
-            element.style.height= (h)+'px';
+        if (h > element.offsetHeight || h < element.offsetHeight - 48)
+            element.style.height = (h) + 'px';
 
-        else if (h<element.offsetHeight || h>element.offsetHeight-48)
-            element.style.height= (h)+'px';
+        else if (h < element.offsetHeight || h > element.offsetHeight - 48)
+            element.style.height = (h) + 'px';
     }
 
-    element.onkeyup= update;
+    element.onkeyup = update;
     setInterval(update, 500);
     update();
 
@@ -28,6 +41,10 @@ function stretchy(element) {
 stretchy(document.getElementById('input-text'));
 
 async function openai_test(text) {
+
+    $(".loading").append(`<div><div class="fa-5x loading_class">
+  <i class="fa fa-spinner fa-spin fa-pulse"></i></div></div>`)
+
     var url = "https://api.openai.com/v1/engines/text-davinci-002/completions";
 
     var data = `{
@@ -45,17 +62,18 @@ async function openai_test(text) {
         "url": url,
         "method": "POST",
         "headers": {
-            "Authorization": "Bearer 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",
+            "Authorization": "Bearer {API}",
             "Content-Type": "application/json",
         },
         data: data,
         success: function (response) {
             text = response.choices[0].text
             result = text.replaceAll("\n", "<br>")
-            console.log(response.choices[0])
-            console.log(result)
             $("#result-text").html(`<p>${result}<br><br></p>`)
-            
+            $(".loading_class").remove()
+            $("#input-text").val("Write a")
+
+
         },
         error: function (error) {
             $("#result-text").html(`<p><i>Sorry. Unable to compile</i></p>`)
@@ -63,5 +81,6 @@ async function openai_test(text) {
     })
 }
 
+$(".loading_class").remove()
 $(".result").destroy
 $("#result-text").html(`<p id="result" style={}>${result}</p>`)
